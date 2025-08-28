626d6c86e7971ded3e062bf613988f9d244c0ada
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2025-08-28 09:58:42 +0200

    date?

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from healthcheck import HealthCheck
 from prometheus_client import generate_latest
-
+from datetime import datetime
 from utils.logging import set_logging_configuration, is_ready_gauge, last_updated_gauge
 from utils.config import DEBUG, PORT, POD_NAME
 from job_endpoints import job_api_bp
@@ -29,4 +29,8 @@ app = create_app()
 
 
 if __name__ == '__main__':  # pragma: no cover
+    date = datetime.today()
+    print(date.strftime("%Y-%m-%d"))
     app.run(debug=DEBUG, host='0.0.0.0', port=PORT)
+    # from jobs.nexus_flow_brugerauth import job
+    # job()
